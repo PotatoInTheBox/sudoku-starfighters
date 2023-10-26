9f087f76a9648dc4578a01b9343a7baf6477152f
tmobreno <tmobreno@gmail.com>
2023-10-26 14:19:07 -0700

    Update Player and Entity
    added methods from class diagram

--- src/model/Entity.java
+++ src/model/Entity.java
@@ -2,5 +2,28 @@ package model;
 
 public abstract class Entity
 {
+	protected float positionX;
+	protected float positionY;
+	protected float sizeX;
+	protected float sizeY;
+	
+	public float getPositionX() {
+		return positionX;
+	}
+	
+	public float getPositionY() {
+		return positionY;
+	}
+	
+	public float getSizeX() {
+		return sizeX;
+	}
+	
+	public float getSizeY() {
+		return sizeY;
+	}
 
+	public boolean hasCollidedWith(Entity other) {
+		return false;
+	}
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -2,5 +2,23 @@ package model;
 
 public class Player extends Entity
 {
-
+	public Player() {
+		
+	}
+	
+	public void moveLeft() {
+		
+	}
+	
+	public void moveRight() {
+		
+	}
+	
+	public boolean isHit() {
+		return false;
+	}
+	
+	public void shootBullet() {
+		
+	}
 }
