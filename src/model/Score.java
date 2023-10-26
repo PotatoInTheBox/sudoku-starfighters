d224294043c182ddac53b190a97a83bf69ab6aa6
tmobreno <tmobreno@gmail.com>
2023-10-26 14:11:37 -0700

    Update Score.java
    added storage for score

--- src/model/Score.java
+++ src/model/Score.java
@@ -2,5 +2,17 @@ package model;
 
 public class Score
 {
-
+	private int score;
+	
+	public Score() {
+		score = 0;
+	}
+	
+	public int getScore(){
+		return score;
+	}
+	
+	public void changeScore(int change) {
+		score = score + change;
+	}
 }
