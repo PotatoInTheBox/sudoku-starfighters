f517866b521254483a7ee56f9bc2b677fe8d4eb1
tmobreno <tmobreno@gmail.com>
2023-11-01 16:56:16 -0700

    Fixed spacing on Leaderboard
    small fix

--- src/view_controller/LeaderboardPane.java
+++ src/view_controller/LeaderboardPane.java
@@ -17,67 +17,67 @@ import javafx.scene.layout.VBox;
 import model.Score;
 
 public class LeaderboardPane extends GridPane {
-	
+
 	private ArrayList<Score> topScores = new ArrayList<Score>();
 	private VBox pane;
-	
-    private Button backButton;
-    private List<EventHandler<ActionEvent>> backHandlers = new ArrayList<>();
-	
-	public LeaderboardPane() {
-        setAlignment(Pos.CENTER);
-        
-        backButton = new Button("Back");
-        
-        Score t = new Score();
-        t.setUsername("tim");
-        t.changeScore(10);
-        Score s = new Score();
-        s.setUsername("sal");
-        s.changeScore(1990);
-        Score g = new Score();
-        g.setUsername("gus");
-        g.changeScore(100);
-        topScores.add(t);
-        topScores.add(s);
-        topScores.add(g);
-        
-		pane = new VBox();
-		pane.setPadding(new Insets(10, 10, 10, 10));
-		pane.setSpacing(10);
-		
-        backButton.setOnAction(e -> {
-            for (EventHandler<ActionEvent> event : backHandlers)
-                event.handle(e);
-        });
 
-        updateScores();
-	}
-	
-    public void onBack(EventHandler<ActionEvent> eventHandler) {
-        backHandlers.add(eventHandler);
-    }
-    
-    public void updateScores() {
-    	getChildren().clear();
+	private Button backButton;
+	private List<EventHandler<ActionEvent>> backHandlers = new ArrayList<>();
+
+	public LeaderboardPane() {
+		setAlignment(Pos.CENTER);
+
+		backButton = new Button("Back");
+
+		Score t = new Score();
+		t.setUsername("tim");
+		t.changeScore(10);
+		Score s = new Score();
+		s.setUsername("sal");
+		s.changeScore(1990);
+		Score g = new Score();
+		g.setUsername("gus");
+		g.changeScore(100);
+		topScores.add(t);
+		topScores.add(s);
+		topScores.add(g);
+
 		pane = new VBox();
 		pane.setPadding(new Insets(10, 10, 10, 10));
 		pane.setSpacing(10);
-        Collections.sort(topScores, new Comparator<Score>() {
-            @Override
-            public int compare(Score score1, Score score2) {
-                return Integer.compare(score2.getScore(), score1.getScore());
-            }
-        });
-        
-        int i = 0;
-        for(Score score : topScores){
-        	if(i<10) {
-            	Label cur = new Label(score.getUsername() + " : " + Integer.toString(score.getScore()));
-            	pane.getChildren().add(cur);	
-        	}
-        	i++;
-        }
-        addColumn(0, pane, backButton);
-    }
+
+		backButton.setOnAction(e -> {
+			for (EventHandler<ActionEvent> event : backHandlers)
+				event.handle(e);
+		});
+
+		updateScores();
+	}
+
+	public void onBack(EventHandler<ActionEvent> eventHandler) {
+		backHandlers.add(eventHandler);
+	}
+
+	public void updateScores() {
+		getChildren().clear();
+		pane = new VBox();
+		pane.setPadding(new Insets(10, 10, 10, 10));
+		pane.setSpacing(10);
+		Collections.sort(topScores, new Comparator<Score>() {
+			@Override
+			public int compare(Score score1, Score score2) {
+				return Integer.compare(score2.getScore(), score1.getScore());
+			}
+		});
+
+		int i = 0;
+		for (Score score : topScores) {
+			if (i < 10) {
+				Label cur = new Label(score.getUsername() + " : " + Integer.toString(score.getScore()));
+				pane.getChildren().add(cur);
+			}
+			i++;
+		}
+		addColumn(0, pane, backButton);
+	}
 }
--- src/view_controller/MainGUI.java
+++ src/view_controller/MainGUI.java
@@ -9,77 +9,77 @@ import javafx.stage.Stage;
 
 public class MainGUI extends Application {
 
-    GamePane gamePane;
-    MenuPane menuPane;
-    LeaderboardPane leaderboardPane;
-    Input input;
+	GamePane gamePane;
+	MenuPane menuPane;
+	LeaderboardPane leaderboardPane;
+	Input input;
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Space Invaders");
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 600, 600);
-        this.input = new Input(scene);
-        primaryStage.setScene(scene);
-        loadCssStyleFile(scene);
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Space Invaders");
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 600, 600);
+		this.input = new Input(scene);
+		primaryStage.setScene(scene);
+		loadCssStyleFile(scene);
 
-        menuPane = new MenuPane();
-        leaderboardPane = new LeaderboardPane();
+		menuPane = new MenuPane();
+		leaderboardPane = new LeaderboardPane();
 
-        menuPane.onNewGame(e -> {
-            gamePane = new GamePane(scene, input, 600, 600);
-            root.setCenter(gamePane);
-        });
-        menuPane.onContinueGame(e -> {
-            if (gamePane != null) {
-                root.setCenter(gamePane);
-                gamePane.unpauseGame();
-            } else {
-                System.err.println("No game currently exists!");
-            }
-        });
-        menuPane.onLeaderboard(e -> {
-        	leaderboardPane.updateScores();
-            root.setCenter(leaderboardPane);
-        });
-        menuPane.onOptions(e -> {
-            System.out.println("Unimplemented");
-        });
-        menuPane.onExit(e -> {
-            Platform.exit();
-            System.exit(0);
-        });
-        leaderboardPane.onBack(e -> {
-            root.setCenter(menuPane);
-        });
+		menuPane.onNewGame(e -> {
+			gamePane = new GamePane(scene, input, 600, 600);
+			root.setCenter(gamePane);
+		});
+		menuPane.onContinueGame(e -> {
+			if (gamePane != null) {
+				root.setCenter(gamePane);
+				gamePane.unpauseGame();
+			} else {
+				System.err.println("No game currently exists!");
+			}
+		});
+		menuPane.onLeaderboard(e -> {
+			leaderboardPane.updateScores();
+			root.setCenter(leaderboardPane);
+		});
+		menuPane.onOptions(e -> {
+			System.out.println("Unimplemented");
+		});
+		menuPane.onExit(e -> {
+			Platform.exit();
+			System.exit(0);
+		});
+		leaderboardPane.onBack(e -> {
+			root.setCenter(menuPane);
+		});
 
-        input.onKeyDown(e -> {
-            if (e.getCode().equals(KeyCode.ESCAPE)) {
-                gamePane.pauseGame();
-                root.setCenter(menuPane);
-            }
-        });
+		input.onKeyDown(e -> {
+			if (e.getCode().equals(KeyCode.ESCAPE)) {
+				gamePane.pauseGame();
+				root.setCenter(menuPane);
+			}
+		});
 
-        root.setCenter(menuPane);
-        primaryStage.show();
-    }
+		root.setCenter(menuPane);
+		primaryStage.show();
+	}
 
-    /**
-     * Load CSS styles from a file if available and apply them to the scene.
-     * 
-     * @param scene The JavaFX scene to which CSS styles should be applied.
-     */
-    private void loadCssStyleFile(final Scene scene) {
-        try {
-            final String css = "file:./resources/css/darkStyle.css";
-            scene.getStylesheets().add(css);
-        } catch (final Exception e) {
-            System.out.println("Unable to load style.css");
-        }
+	/**
+	 * Load CSS styles from a file if available and apply them to the scene.
+	 * 
+	 * @param scene The JavaFX scene to which CSS styles should be applied.
+	 */
+	private void loadCssStyleFile(final Scene scene) {
+		try {
+			final String css = "file:./resources/css/darkStyle.css";
+			scene.getStylesheets().add(css);
+		} catch (final Exception e) {
+			System.out.println("Unable to load style.css");
+		}
 
-    }
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
