4aba9576c7d9a3686bf7f195b4203ca5223311b4
PotatoInTheBox <ivangusachenko3@gmail.com>
2023-11-13 21:35:10 -0700

    improve framerate tracking

--- src/view_controller/FrameRateTracker.java
+++ src/view_controller/FrameRateTracker.java
@@ -6,6 +6,7 @@ public class FrameRateTracker {
     private int fpsCounter = 0;
     private final int bufferSize; // buffer size
     private long[] fpsRecord;
+    private boolean isStarting = true;
 
     public FrameRateTracker(int frameCounterBufferSize) {
         bufferSize = frameCounterBufferSize;
@@ -14,17 +15,37 @@ public class FrameRateTracker {
 
     public void logFrameUpdate() {
         long thisTime = System.nanoTime();
+        // don't record if a frame took longer than one second
+        if (thisTime - lastFpsTime > 1_000_000_000d) {
+            lastFpsTime = thisTime;
+            return;
+        }
         fpsRecord[fpsCounter % bufferSize] = (thisTime - lastFpsTime);
         lastFpsTime = thisTime;
         fpsCounter++;
+        if (bufferSize == fpsCounter) {
+            isStarting = false;
+        }
     }
 
     public double getAverageUpdate() {
+        if (isStarting && fpsCounter == 0) {
+            return 0;
+        }
         long total = 0l;
-        for (int i = 0; i < bufferSize; i++) {
+        for (int i = 0; i < bufferSize && !isStarting; i++) {
             total += fpsRecord[i];
         }
-        double average = 1000d / ((double) (total / bufferSize) / 1_000_000d);
-        return average;
+        for (int i = 0; i < fpsCounter % bufferSize && isStarting; i++) {
+            total += fpsRecord[i];
+        }
+        if (!isStarting) {
+            double average = 1000d / (((double) total / bufferSize) / 1_000_000d);
+            return average;
+        } else {
+            double average = 1000d / (((double) total / (fpsCounter % bufferSize)) / 1_000_000d);
+            return average;
+        }
+
     }
 }
